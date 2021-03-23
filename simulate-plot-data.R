54a1db4ee6f59b022367304f9e9c22d105528ffd
Erin <etracy1121@gmail.com>
2021-03-23 10:28:53 -0700

    changed sd

--- simulate-plot-data.R
+++ simulate-plot-data.R
@@ -5,7 +5,9 @@
 
 #Simulate data from normal distribution
 x <- rnorm(n = 100)
-y <- 2 * x + rnorm(n = 100, sd= 0.1)
+y <- 2 * x + rnorm(n = 100, sd= 0.2)
 
 #plot simulated data
 plot(x = x, y = y)
+#save file, add by clicking box in github tab, 
+#click commit, write a commit message and commit, push (green arrow) to github
